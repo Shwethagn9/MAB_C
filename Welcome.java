01ef8c2ff8851e4f6ecf025ad796db7df9645dcc
Shwetha <gn.shwetha12@gmail.com>
2024-05-31 11:45:35 +0530

    secondcommit

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package boi.com.a;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                System.out.println("Welcome");
+	}
+
+}
